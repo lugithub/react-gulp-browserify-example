949a19216b68166af32adc7b553283e1ed68f7ca
lubin <lu.bin.lu.bin@gmail.com>
2015-05-02 16:57:08 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 
 gulp.task('default', function(){
-    var bundle = browserify({entries: './src/app.js'})
+    var bundle = browserify({entries: './src/app.jsx'})
       .transform('reactify')
-      .bundle({ debug: true })
+      .bundle()
       .pipe(source('deps.min.js'))
       .pipe(gulp.dest('dist'));
 });
